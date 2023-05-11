4ef1532636e8d35446a018fc8359b9db1d3a63db
Joao <j_donizetti@outlook.com>
2023-05-11 17:28:37 -0300

    debug code

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,15 +1,9 @@
-
 const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
 acordeonTriggers.forEach((trigger) => {
     trigger.addEventListener('click', (e) => {
         const acordeon = trigger.parentElement
-        const isOpen = acordeon.classList.contains('open')
+        acordeon.classList.toggle('open')
 
-        if (isOpen) {
-            acordeon.classList.remove('open')
-        } else {
-            acordeon.classList.add('open')
-        }
     })
 })
--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,5 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/jmdonizetti/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    const response = await fetch('https://raw.githubusercontent.com/jmdonizetti/js-developer-portfolio/main/data/profile.json')
+    return response.json()
 }
